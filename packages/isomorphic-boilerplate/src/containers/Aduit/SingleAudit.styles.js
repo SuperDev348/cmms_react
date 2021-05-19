f5b30e8e18b235e4f81a28d40b5cf58f642e0bf1
VladimirDanil <vladimir.danil25@mail.ru>
2021-05-19 07:35:45 -0700

    upadte

--- packages/isomorphic-boilerplate/src/containers/Aduit/SingleAudit.styles.js
+++ packages/isomorphic-boilerplate/src/containers/Aduit/SingleAudit.styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { palette } from 'styled-theme';
 import WithDirection from '@iso/lib/helpers/rtl';          
-
+     
 const PageWrapper = styled.div`
   .PageHeader {
     width: 100%;
