181cbcf78de5c89dac5afb9feff3440baa7fe707
VladimirDanil <vladimir.danil25@mail.ru>
2021-08-20 06:27:51 -0700

    fix

--- packages/isomorphic-boilerplate/src/containers/Business/ViewInvoice.js
+++ packages/isomorphic-boilerplate/src/containers/Business/ViewInvoice.js
@@ -1,4 +1,4 @@
-import React from 'react';   
+import React from 'react'; 
 import { Link } from 'react-router-dom';
 import Box from '@iso/components/utility/box';
 import Button from '@iso/components/uielements/button';
