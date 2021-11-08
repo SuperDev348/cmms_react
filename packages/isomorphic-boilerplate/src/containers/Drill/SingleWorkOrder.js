5a1672c178550ecad443816739813383ada8db8b
VladimirDanil <vladimir.danil25@mail.ru>
2021-11-08 08:59:11 -0800

    some update

--- packages/isomorphic-boilerplate/src/containers/Drill/SingleWorkOrder.js
+++ packages/isomorphic-boilerplate/src/containers/Drill/SingleWorkOrder.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react';   
 import {  useSelector } from 'react-redux';
 import {  useParams } from 'react-router-dom';
 import AddWorkOrder from './AddWorkOrder';
--- packages/isomorphic-boilerplate/src/containers/Drill/SingleWorkOrder.styles.js
+++ packages/isomorphic-boilerplate/src/containers/Drill/SingleWorkOrder.styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components';   
 import { palette } from 'styled-theme';
 import WithDirection from '@iso/lib/helpers/rtl';
 
